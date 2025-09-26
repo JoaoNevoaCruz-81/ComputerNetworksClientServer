1c65fbe231b1734edc395603703e0ba0da5b4502
Bernardo <b.lobao@campus.fct.unl.pt>
2025-09-26 18:15:37 +0100

    Update on the server.py file

--- server.py
+++ server.py
@@ -1,56 +1,120 @@
 import sys
-from socket import *
+import os
 import threading
+from socket import *
+import pickle
 
-server_ip = "172.17.0.2"
-welcome_message = f"Welcome to {server_ip} file server"
-bufferSize = 512
+SERVER_IP = "172.17.0.2"
+WELCOME_MESSAGE = f"Welcome to {SERVER_IP} file server"
+WELCOME_BLOCKS = 1
+BUFFER_SIZE = 512
+MAX_PACKAGE_SIZE = 515
+DIRECTORY_PATH = "."
+DIR_COMMAND = ""
+
+RRQ = 1
+DAT = 3
+ACK = 4
+ERR = 5
+
+error = False
+ended = False
+
+dir_list = os.listdir(DIRECTORY_PATH)
+
+def closeSocket(sock):
+    sock.close()
+
+def acknowledge(sock, block_count):
+    package = sock.recv(MAX_PACKAGE_SIZE)
+    opcode, block_number = pickle.loads(package)
+    if not (opcode == ACK and block_number == block_count):
+        error = True
 
 
-def execute_command(cmd):
-    match cmd:
-        case "end":
-            print("Goodbye!")
-        case "dir":
-            print("Dir command")
-        case "get":
-            print("Get command")
+def send_dirs(sock):
+    block_count = 0
+    for path in os.listdir(DIRECTORY_PATH):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(DIRECTORY_PATH, path)):
+            block_count += 1
+            package = (DAT, block_count, len(path), path)
+            sock.send(pickle.dump(package))
+    # Empty last block to signal transfer is over
+    sock.send((DAT, block_count, 0, ""))
 
+def send_file(filename, sock):
+    file = open(filename, "rb")
+    file_size = os.path.getsize(filename)
+    total_blocks = (file_size/BUFFER_SIZE) + 1
+    block_count = 1
+    acknowledged = True
+    while block_count <= total_blocks and acknowledged:
+        if block_count == total_blocks:
+            size = file_size - (BUFFER_SIZE * total_blocks)
+        else:
+            size = BUFFER_SIZE
 
-def handle_client(socket):
-    socket.send(welcome_message.encode())
+        file_data = file.read(BUFFER_SIZE)
+        requested_package = (DAT, block_count, size, file_data)
+        sock.send(pickle.dumps(requested_package))
 
-    while True:
-        command = socket.recv(bufferSize).decode()
-        execute_command(command)
-        if command == "end":
+        acknowledge(sock, block_count)
+
+        if error:
             break
+        block_count += 1
+
+
+
+def analyse_package(op_code, package, sock):
+    if op_code == RRQ:
+        filename = package[1]
+        if filename == DIR_COMMAND:
+            send_dirs(sock)
+        else:
+            print("File requested is ", filename)
+            send_file(filename, sock)
+    else:
+        error = True
+
+
+def handle_client(sock):
+    # Welcome message
+    welcome_package = (DAT, WELCOME_BLOCKS, len(WELCOME_MESSAGE), WELCOME_MESSAGE)
+    sock.send(pickle.dumps(welcome_package))
+    acknowledge(sock, WELCOME_BLOCKS)
+
+    # Process packages
+    while not (error or ended):
+        received_package = pickle.loads(sock.recv(MAX_PACKAGE_SIZE))
+        op_code = received_package[0]
+        analyse_package(op_code, received_package, sock)
 
 
 def main():
-
     if len(sys.argv) != 2 :
         print("Error: Incorrect number of arguments.")
         sys.exit(1)
+    try:
+        server_port = sys.argv[1]
+        serverSocket = socket(AF_INET,SOCK_STREAM)
 
-    server_port = sys.argv[1]
+        serverSocket.bind(SERVER_IP, server_port)
+        serverSocket.listen(5)
 
+        print("Server is running")
 
-    serverSocket = socket(AF_INET,SOCK_STREAM)
+        while True:
+            connSocket, client_addr = serverSocket.accept()
 
-    serverSocket.bind(server_ip, server_port)
+            print("Connected to: ", str(client_addr))
+            tid = threading.Thread(target=handle_client, args=(connSocket))
+            tid.start()
 
-    serverSocket.listen(5)
-    print("Server is running")
-
-    while True:
-        connSocket, client_addr = serverSocket.accept()
-
-        print("Connected to: ", str(client_addr))
-        tid = threading.Thread(target=handle_client, args=(connSocket))
-        tid.start()
-
-        connSocket.close()
+            connSocket.close()
+    except:
+        print("Unable to start server")
 
 
 main()
